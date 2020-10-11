36f7b5f68ffbcc4a3208125423743f95c0149e57
vitalchip <65342577+vitalchip@users.noreply.github.com>
2020-10-11 12:17:11 +0100

    Update PlayerInfoPanel.cpp

--- source/PlayerInfoPanel.cpp
+++ source/PlayerInfoPanel.cpp
@@ -621,7 +621,7 @@ void PlayerInfoPanel::DrawFleet(const Rectangle &bounds)
 		table.Draw(hull);
 		
 		string fuel = to_string(static_cast<int>(
-			ship.Attributes().Get("fuel capacity") * ship.Fuel()));
+			ship.Attributes().Get("cargo space")));
 		table.Draw(fuel);
 		
 		// If this isn't the flagship, we'll remember how many crew it has, but
