20a195c8815897f585f49f3f008d789b51bfd733
vitalchip <65342577+vitalchip@users.noreply.github.com>
2020-10-14 14:19:05 +0100

    Update ShipInfoPanel.cpp

--- source/ShipInfoPanel.cpp
+++ source/ShipInfoPanel.cpp
@@ -72,6 +72,7 @@ void ShipInfoPanel::Draw()
 	// Fill in the information for how this interface should be drawn.
 	Information interfaceInfo;
 	interfaceInfo.SetCondition("ship tab");
+	canEdit = 'true';
 	if(canEdit && (shipIt != player.Ships().end())
 			&& (shipIt->get() != player.Flagship() || (*shipIt)->IsParked()))
 	{
@@ -139,6 +140,7 @@ bool ShipInfoPanel::KeyDown(SDL_Keycode key, Uint16 mod, const Command &command,
 	}
 	else if(key == 'R' || (key == 'r' && shift))
 		GetUI()->Push(new Dialog(this, &ShipInfoPanel::Rename, "Change this ship's name?"));
+	canEdit = 'true';
 	else if(canEdit && (key == 'P' || (key == 'p' && shift)))
 	{
 		if(shipIt->get() != player.Flagship() || (*shipIt)->IsParked())
