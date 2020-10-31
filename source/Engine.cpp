21d7cb278b049d3f6ecb4c192d07374fa90c5e42
vitalchip <65342577+vitalchip@users.noreply.github.com>
2020-10-31 16:21:29 +0000

    Update Engine.cpp

--- source/Engine.cpp
+++ source/Engine.cpp
@@ -1713,6 +1713,12 @@ void Engine::HandleKeyboardInputs()
 		activeCommands |= Command::STOP;
 		activeCommands.Clear(Command::BACK);
 	}
+	// Command to cloak entire fleet
+	if(keyHeld.Has(Command::CLOAK) && keyHeld.Has(Command::SHIFT))
+	{
+		activeCommands |= Command::CLOAKFLEET;
+		//activeCommands.Clear(Command::CLOAK);
+	}	
 }
 
 
