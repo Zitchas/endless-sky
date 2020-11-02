3c5e6e1dfd7fcbf1c48a8ad0d56e5b82defce93e
vitalchip <65342577+vitalchip@users.noreply.github.com>
2020-11-02 23:31:22 +0000

    Update PreferencesPanel.cpp

--- source/PreferencesPanel.cpp
+++ source/PreferencesPanel.cpp
@@ -390,13 +390,13 @@ void PreferencesPanel::DrawControls()
 	Table shiftTable;
 	shiftTable.AddColumn(125, Table::RIGHT);
 	shiftTable.SetUnderline(0, 130);
-	shiftTable.DrawAt(Point(-400, 0));
+	shiftTable.DrawAt(Point(-400, -2));
 	
 	shiftTable.DrawUnderline(medium);
 	shiftTable.Draw("With <shift> key", bright);
 	shiftTable.DrawGap(5);
 	shiftTable.Draw("Cloak flagship", medium);
-	shiftTable.DrawGap(25);	
+	shiftTable.DrawGap(30);	
 	shiftTable.Draw("Select nearest ship", medium);
 	shiftTable.Draw("Select next escort", medium);
 	shiftTable.Draw("Talk to planet", medium);
