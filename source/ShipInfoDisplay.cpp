10a8526dc1d105d62be76e2016386151ac0eae1e
Zitchas <32169904+Zitchas@users.noreply.github.com>
2024-12-29 22:31:20 -0500

    Deleting uneeded variables.

--- source/ShipInfoDisplay.cpp
+++ source/ShipInfoDisplay.cpp
@@ -737,16 +737,10 @@ void ShipInfoDisplay::DrawShipManeuverStats(const Ship &ship, const Rectangle & 
 	fullMass /= reduction;
 
 	double lateralRatioThrust = 0.;
-	double lateralRatioEnergy = 0.;
-	double lateralRatioHeat = 0.;
 	double lateralCombinedThrust = 0.;
 	if(attributes.Get("lateral thrust ratio"))
-	{
 		lateralRatioThrust = attributes.Get("lateral thrust ratio") * attributes.Get("thrust");
-		lateralRatioEnergy = attributes.Get("lateral thrust ratio") * attributes.Get("thrusting energy");
-		lateralRatioHeat = attributes.Get("lateral thrust ratio") * attributes.Get("thrusting heat");
-	}
-	double lateralCombinedThrust = (attributes.Get("lateral thrust") + lateralRatioThrust);
+	lateralCombinedThrust = (attributes.Get("lateral thrust") + lateralRatioThrust);
 
 	double baseAccel = 3600. * attributes.Get("thrust") * (1. + attributes.Get("acceleration multiplier"));
 	double afterburnerAccel = 3600. * attributes.Get("afterburner thrust") * (1. +
